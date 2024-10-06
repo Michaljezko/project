88f84cae9680d50d5f0c8c61d174e8d7d6e5e37b
Michaljezko <mariancik.michal@gmail.com>
2024-10-07 00:36:25 +0200

    I repair py, first FOOBAR then FOO and BAR

--- Resources/Variables.py
+++ Resources/Variables.py
@@ -12,12 +12,21 @@ print(sum(random_numbers))
 
 
 
-for random_number in range(1, 10):
-    if random_number % 3 ==0:
-        print("FOO")
+for random_number in range(1, 100):
+    if random_number % 3 == 0 and random_number % 5 == 0:
+        print("FOOBAR")
+    elif random_number % 3 == 0:
+        print("FOO")  
     elif random_number % 5 == 0:
-        print("BAR")  
-    elif random_number % 3 == 0 and random_number % 5 == 0:
-        print("FOOBAR")     
-
-
+        print("BAR")     
+    else:
+        print(random_number)  
+#for one_number in range(0, 100):
+#     if one_number % 3 == 0 and one_number % 5 == 0:
+#         print("FizzBuzz")
+#     elif one_number % 3 == 0:
+#         print("Fizz")    
+#     elif one_number % 5 == 0:
+#         print("Buzz")    
+#     else:
+#         print(one_number) 
